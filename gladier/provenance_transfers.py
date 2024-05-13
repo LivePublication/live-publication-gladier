bb73642683869aa96f88a603a6e90974982e49f2
Nelis Drost <nelis.drost@gmail.com>
2024-05-13 13:59:21 +1200

    Make DistCrateTransfer class responsible for generating its flow definition, rather than ProvenanceBaseClient

--- gladier/provenance_transfers.py
+++ gladier/provenance_transfers.py
@@ -1,4 +1,9 @@
+from typing import Mapping, Any
+
 from gladier_tools.globus import Transfer
+
+from gladier.utils.name_generation import get_upper_camel_case
+from gladier.utils.tool_alias import ToolAlias, StateSuffixVariablePrefix
 
 
 class DistCrateTransfer(Transfer):
@@ -16,3 +21,36 @@ class DistCrateTransfer(Transfer):
         'orchestration_server_endpoint_id',
         '_provenance_crate_destination_directory',
     ]
+
+    def __init__(self, func_name: str):
+        alias = f'_provenance_{func_name}'
+        super().__init__(alias, StateSuffixVariablePrefix)
+        # TODO: we're making assumptions about both the function name, and it's ResultPath
+        #   this is currently necessary as we don't have access to the previous state in get_flow_definition()
+        self.func_name = get_upper_camel_case(func_name)
+
+
+    def get_flow_definition(self) -> Mapping[str, Any]:
+        flow_definition = super().get_flow_definition()
+        for state_name, state_data in flow_definition['States'].items():
+            state_data['ResultPath'] = f'$.{state_name}'
+            transfer_parameters = state_data['Parameters']
+            transfer_items = transfer_parameters['transfer_items']
+
+            # Source and destination endpoints may be set dynamcially
+
+            # Transfer items (Distriubted Step Crates) are always
+            # named {task_id}.crate
+
+            transfer_items[0].pop('recursive.$', None)
+            transfer_items[0]['recursive'] = True
+
+            transfer_items[0].pop('source_path.$', None)
+            transfer_items[0]['source_path.='] = f"`$.{self.func_name}.details.results[0].task_id` + '.crate'"
+            transfer_items[0].pop('destination_path.$', None)
+            transfer_items[0]['destination_path.='] = f"`$.input._provenance_crate_destination_directory` + '/' + `$.{self.func_name}.details.results[0].task_id`"
+
+            transfer_parameters['source_endpoint_id.$'] = '$.input.prov_compute_GCS_id'
+            transfer_parameters['destination_endpoint_id.$'] = '$.input.orchestration_server_endpoint_id'
+
+        return flow_definition
